45f1d7978bd6835ec3ca677fccb6dcffcd855fe9
Logan McAnsh <logan@mcan.sh>
2017-06-24 18:22:46 -0400

    Initial Commit
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ Readme.markdown
@@ -0,0 +1,21 @@
+# Blog Redirect
+
+Simple Express app to redirect old blog post URLs to my new blog.
+
+## Running Locally
+
+First, install the dependencies:
+
+    $ yarn
+
+Now, start the server:
+
+    $ yarn dev
+
+How you can visit the site at <http://localhost:3000>
+
+## Testing
+
+Simply run the following command to run the tests:
+
+    $ yarn test
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const url = require('url');
+
+const server = express();
+
+server.get('*', (req, res) => {
+  res.redirect(301, url.format({
+    protocol: 'https',
+    host: 'mcansh.blog',
+    pathname: req.path,
+    query: req.query
+  }));
+});
+
+server.listen(3000, (err) => {
+  if (err) throw err;
+  console.log('> Ready on http://localhost:3000');
+});
+
+module.exports = server;
