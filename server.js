36ea674de70f69cb794e07a6812d9dc1c63a021e
Logan McAnsh <logan@mcan.sh>
2017-09-25 14:01:17 -0400

    run against prettier

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const url = require('url');
 const server = express();
 
 server.get('*', (req, res) => {
-  res.redirect(301, url.format({
-    protocol: 'https',
-    host: 'mcansh.blog',
-    pathname: req.path,
-    query: req.query
-  }));
+  res.redirect(
+    301,
+    url.format({
+      protocol: 'https',
+      host: 'mcansh.blog',
+      pathname: req.path,
+      query: req.query,
+    }),
+  );
 });
 
 server.listen(3000, (err) => {
